6856f5d5a8d6ab77cf1e66b3fdbf2d1def971d55
Xiangyang Ju <xiangyang.ju@gmail.com>
2017-05-15 13:22:05 +0200

    add

---
+++ root_plot_utils/adder.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import ROOT
+
+class adder:
+    """
+    Add line/text into canvas
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add_text(self, x, y, color, text, font=42):
+        l = ROOT.TLatex()
+        l.SetTextSize(self.text_size)
+        l.SetNDC()
+        l.SetTextColor(color)
+        l.SetTextFont(font)
+        l.DrawLatex(x, y, text)
+
+    @staticmethod
+    def add_line(hist, y_val, color=1, style=2, option="x"):
+        x_low = hist.GetBinLowEdge(hist.GetXaxis().GetFirst())
+        x_hi = hist.GetBinLowEdge(hist.GetXaxis().GetLast()+1)
+        y_low = hist.GetBinLowEdge(hist.GetYaxis().GetFirst())
+        y_hi = hist.GetBinLowEdge(hist.GetYaxis().GetLast()+1)
+        line = ROOT.TLine()
+        line.SetLineColor(color)
+        line.SetLineStyle(style)
+        line.SetLineWidth(2)
+        if option.lowercase() == "x":
+            line.DrawLine(x_low, y_val, x_hi, y_val)
+        else:
+            line.DrawLine(y_val, y_low, y_val, y_hi)
+        
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='root_plot_utils',
+      version='0.1',
+      description='utilities for plotting in ROOT',
+      url='https://github.com/xju2/root_plot_utils',
+      author='Xiangyang Ju',
+      author_email='xiangyang.ju@gmail.com',
+      license='MIT',
+      packages=['root_plot_utils'],
+      zip_safe=False
+     )
