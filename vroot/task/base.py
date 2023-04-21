f15bc2abd509c205755637920b6dba7eed59fb0c
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-04-20 23:51:28 -0700

    tasks

---
+++ vroot/task/base.py
@@ -0,0 +1,11 @@
+from vroot.hparams_mixin import HyperparametersMixin
+
+class TaskHooks:
+
+    def run(self) -> None:
+        """Run the task"""
+
+
+class TaskBase(TaskHooks, HyperparametersMixin):
+    def __init__(self) -> None:
+        super().__init__()
---
+++ vroot/utils/utils.py
@@ -0,0 +1,107 @@
+import time
+import warnings
+from pathlib import Path
+from typing import Callable, List, Any
+
+import hydra
+from omegaconf import DictConfig
+
+from vroot.utils import pylogger, rich_utils
+
+log = pylogger.get_pylogger(__name__)
+
+
+def task_wrapper(task_func: Callable) -> Callable:
+    """Optional decorator that wraps the task function in extra utilities.
+    Makes multirun more resistant to failure.
+    Utilities:
+    - Calling the `utils.extras()` before the task is started
+    - Calling the `utils.close_loggers()` after the task is finished
+    - Logging the exception if occurs
+    - Logging the task total execution time
+    - Logging the output dir
+    """
+
+    def wrap(cfg: DictConfig):
+
+        # apply extra utilities
+        extras(cfg)
+
+        # execute the task
+        try:
+            start_time = time.time()
+            task_func(cfg=cfg)
+        except Exception as ex:
+            log.exception("")  # save exception to `.log` file
+            raise ex
+        finally:
+            path = Path(cfg.paths.output_dir, "exec_time.log")
+            content = f"'{cfg.task_name}' execution time: {time.time() - start_time} (s)"
+            save_file(path, content)  # save task execution time (even if exception occurs)
+            close_loggers()  # close loggers (even if exception occurs so multirun won't fail)
+
+        log.info(f"Output dir: {cfg.paths.output_dir}")
+
+    return wrap
+
+
+def extras(cfg: DictConfig) -> None:
+    """Applies optional utilities before the task is started.
+    Utilities:
+    - Ignoring python warnings
+    - Setting tags from command line
+    - Rich config printing
+    """
+
+    # return if no `extras` config
+    if not cfg.get("extras"):
+        log.warning("Extras config not found! <cfg.extras=null>")
+        return
+
+    # disable python warnings
+    if cfg.extras.get("ignore_warnings"):
+        log.info("Disabling python warnings! <cfg.extras.ignore_warnings=True>")
+        warnings.filterwarnings("ignore")
+
+    # prompt user to input tags from command line if none are provided in the config
+    if cfg.extras.get("enforce_tags"):
+        log.info("Enforcing tags! <cfg.extras.enforce_tags=True>")
+        rich_utils.enforce_tags(cfg, save_to_file=True)
+
+    # pretty print config tree using Rich library
+    if cfg.extras.get("print_config"):
+        log.info("Printing config tree with Rich! <cfg.extras.print_config=True>")
+        rich_utils.print_config_tree(cfg, resolve=True, save_to_file=True)
+
+
+def save_file(path: str, content: str) -> None:
+    """Save file in rank zero mode (only on one process in multi-GPU setup)."""
+    with open(path, "w+") as file:
+        file.write(content)
+
+
+def close_loggers() -> None:
+    """Makes sure all loggers closed properly (prevents logging failure during multirun)."""
+
+    log.info("Closing loggers...")
+
+
+def instantiate_tasks(task_cfg: DictConfig) -> List[Any]:
+    """Instantiates all tasks from config file."""
+
+    tasks = []
+    if not task_cfg:
+        log.warning("Task config is empty!")
+        return []
+
+    if not isinstance(task_cfg, DictConfig):
+        raise TypeError("Task config must be a DictConfig!")
+
+    if "_target_" in task_cfg:
+        tasks.append(hydra.utils.instantiate(task_cfg))
+    else:
+        for _, t_conf in task_cfg.items():
+            if isinstance(t_conf, DictConfig) and "_target_" in t_conf:
+                tasks.append(hydra.utils.instantiate(t_conf))
+
+    return tasks
