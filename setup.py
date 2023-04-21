ba8236289b01098f29103abf5ada8c2dcbe89429
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-04-20 17:20:55 -0700

    vroot

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='root_plot_utils',
+setup(name='vroot',
       version='1.0.1',
-      description='utilities for plotting in ROOT',
+      description='visulization for ROOT',
       url='https://github.com/xju2/root_plot_utils',
       author='Xiangyang Ju',
       author_email='xiangyang.ju@gmail.com',
       license='MIT',
-      packages=['root_plot_utils'],
+      packages=['vroot'],
       zip_safe=False,
       include_package_data=True
       )
