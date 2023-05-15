f6e2367b7201fffcc48dea905b13cfa797f18a9e
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-05-15 14:34:53 -0700

    add canvas and hist config

--- vroot/task/base.py
+++ vroot/task/base.py
@@ -1,6 +1,12 @@
 from vroot.hparams_mixin import HyperparametersMixin
+from omegaconf import DictConfig
 
 class TaskHooks:
+    def add_canvas(self, canvas_config: DictConfig) -> None:
+        """Add canvas to the task"""
+
+    def add_histograms(self, histogram_config: DictConfig) -> None:
+        """Add histograms to the task"""
 
     def run(self) -> None:
         """Run the task"""
