c8226164a8142255eef047afecd31464b8cff96b
Xiangyang Ju <xiangyang.ju@gmail.com>
2023-05-22 14:49:55 -0700

    tail fraction

--- scripts/my_app.py
+++ scripts/my_app.py
@@ -11,12 +11,12 @@ from omegaconf import DictConfig, OmegaConf
 
 track_parameter_names = ["d0", "z0", "qoverp", "pt", "z0sin", "theta", "phi"]
 OmegaConf.register_new_resolver("ex_tp",
-                                lambda x: [x + "_" + y for y in track_parameter_names])
+                                lambda x: [x + y for y in track_parameter_names])
 OmegaConf.register_new_resolver("ex_tp_vals",
                                 lambda x: [x for _ in range(len(track_parameter_names))])
 
 OmegaConf.register_new_resolver("ex_ctp",
-                                lambda x, ys: [x + "_" + y.strip() for y in ys.split(",")])
+                                lambda x, ys: [x + y.strip() for y in ys.split(",")])
 OmegaConf.register_new_resolver("gen_list",
                                 lambda x, y: [x] * y)
 
